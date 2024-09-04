5c9a060f8470f4f0825883541fdfbfc4520c496d
Sam Andrews <137528225+sja980@users.noreply.github.com>
2024-09-04 16:02:54 +0100

    Update rankings_script.js
    Optimized JS for conditional formatting of table cells

--- rankings_script.js
+++ rankings_script.js
@@ -121,209 +121,209 @@ columnDefs: [
 
 //SJR Rankings //
 {targets:[8],
-render: function(data){
-if(parseFloat(data)>=2){
-return "<div class='Top'>"+ data +"</div>";}
+	render: function(data){
+	if(parseFloat(data)>=1.5){
+	return "<div class='topfive'>"+ data +"</div>";}
 
-else if ((parseFloat(data)>=1) && ((parseFloat(data)<2))){
-return "<div class='Great'>"+ data +"</div>";}
+	else if (parseFloat(data)>=0.5){
+	return "<div class='QOne'>"+ data +"</div>";}
 
-else if ((parseFloat(data)>=0.5) && ((parseFloat(data)<1))){
-return "<div class='Good'>"+ data +"</div>";}
+	else if (parseFloat(data)>=0.3){
+	return "<div class='QTwo'>"+ data +"</div>";}
 
-else if ((parseFloat(data)>=0.25) && ((parseFloat(data)<0.5))){
-return "<div class='Ok'>"+ data +"</div>";}
+	else if (parseFloat(data)>=0.15){
+	return "<div class='QThree'>"+ data +"</div>";}
 
-else if ((parseFloat(data)>=0) && ((parseFloat(data)<0.25))){
-return "<div class='Poor'>"+ data +"</div>";}
+	else if (parseFloat(data)>=0){
+	return "<div class='QFour'>"+ data +"</div>";}
 return data }},
 
 // SNIP Rankings //
-{targets:[9], className:"dt-body-center",
-render: function(data){
-if(parseFloat(data)>=2.5){
-return "<div class='Top'>"+ data +"</div>";}
+{targets:[9],
+	render: function(data){
+	if(parseFloat(data)>=2){
+	return "<div class='topfive'>"+ data +"</div>";}
 
-else if ((parseFloat(data)>=2) && ((parseFloat(data)<2.5))){
-return "<div class='Great'>"+ data +"</div>";}
+	else if (parseFloat(data)>=1.25){
+	return "<div class='QOne'>"+ data +"</div>";}
 
-else if ((parseFloat(data)>=1) && ((parseFloat(data)<2))){
-return "<div class='Good'>"+ data +"</div>";}
+	else if (parseFloat(data)>=0.8){
+	return "<div class='QTwo'>"+ data +"</div>";}
 
-else if ((parseFloat(data)>=0.5) && ((parseFloat(data)<1))){
-return "<div class='Ok'>"+ data +"</div>";}
+	else if (parseFloat(data)>=0.35){
+	return "<div class='QThree'>"+ data +"</div>";}
 
-else if ((parseFloat(data)>=0) && ((parseFloat(data)<0.5))){
-return "<div class='Poor'>"+ data +"</div>";}
+	else if (parseFloat(data)>=0){
+	return "<div class='QFour'>"+ data +"</div>";}
 return data }},
 
 // CiSC //
 {targets:[10],
-render: function(data){
-if(parseFloat(data)>=4){
-return "<div class='Top'>"+ data +"</div>";}
+	render: function(data){
+	if(parseFloat(data)>=4){
+	return "<div class='topfive'>"+ data +"</div>";}
 
-else if ((parseFloat(data)>=2) && ((parseFloat(data)<4))){
-return "<div class='Great'>"+ data +"</div>";}
+	else if (parseFloat(data)>=1.7){
+	return "<div class='QOne'>"+ data +"</div>";}
 
-else if ((parseFloat(data)>=1) && ((parseFloat(data)<2))){
-return "<div class='Good'>"+ data +"</div>";}
+	else if (parseFloat(data)>=1){
+	return "<div class='QTwo'>"+ data +"</div>";}
 
-else if ((parseFloat(data)>=0.5) && ((parseFloat(data)<1))){
-return "<div class='Ok'>"+ data +"</div>";}
+	else if (parseFloat(data)>=0.4){
+	return "<div class='QThree'>"+ data +"</div>";}
 
-else if ((parseFloat(data)>=0) && ((parseFloat(data)<0.5))){
-return "<div class='Poor'>"+ data +"</div>";}
+	else if (parseFloat(data)>=0){
+	return "<div class='QFour'>"+ data +"</div>";}
 return data }},
 
 // CTE% //
 {targets:[11],
-render: function(data){
-if(parseFloat(data)>=80){
-return "<div class='Top'>"+ data +"</div>";}
+	render: function(data){
+	if(parseFloat(data)>=70){
+	return "<div class='topfive'>"+ data +"</div>";}
 
-else if ((parseFloat(data)>=65) && ((parseFloat(data)<80))){
-return "<div class='Great'>"+ data +"</div>";}
+	else if (parseFloat(data)>=60){
+	return "<div class='QOne'>"+ data +"</div>";}
 
-else if ((parseFloat(data)>=50) && ((parseFloat(data)<65))){
-return "<div class='Good'>"+ data +"</div>";}
+	else if (parseFloat(data)>=40){
+	return "<div class='QTwo'>"+ data +"</div>";}
 
-else if ((parseFloat(data)>=30) && ((parseFloat(data)<50))){
-return "<div class='Ok'>"+ data +"</div>";}
+	else if (parseFloat(data)>=25){
+	return "<div class='QThree'>"+ data +"</div>";}
 
-else if ((parseFloat(data)>=0) && ((parseFloat(data)<30))){
-return "<div class='Poor'>"+ data +"</div>";}
+	else if (parseFloat(data)>=0){
+	return "<div class='QFour'>"+ data +"</div>";}
 return data }},
 
 // GSH5 //
 {targets:[12],
-render: function(data){
-if(parseFloat(data)>=30){
-return "<div class='Top'>"+ data +"</div>";}
+	render: function(data){
+	if(parseFloat(data)>=30){
+	return "<div class='topfive'>"+ data +"</div>";}
 
-else if ((parseFloat(data)>=20) && ((parseFloat(data)<30))){
-return "<div class='Great'>"+ data +"</div>";}
+	else if (parseFloat(data)>=15){
+	return "<div class='QOne'>"+ data +"</div>";}
 
-else if ((parseFloat(data)>=12) && ((parseFloat(data)<20))){
-return "<div class='Good'>"+ data +"</div>";}
+	else if (parseFloat(data)>=10){
+	return "<div class='QTwo'>"+ data +"</div>";}
 
-else if ((parseFloat(data)>=6) && ((parseFloat(data)<12))){
-return "<div class='Ok'>"+ data +"</div>";}
+	else if (parseFloat(data)>=5){
+	return "<div class='QThree'>"+ data +"</div>";}
 
-else if ((parseFloat(data)>=0) && ((parseFloat(data)<7))){
-return "<div class='Poor'>"+ data +"</div>";}
+	else if (parseFloat(data)>=0){
+	return "<div class='QFour'>"+ data +"</div>";}
 return data }},
 
 // SJRH //
 {targets:[13],
-render: function(data){
-if(parseFloat(data)>=70){
-return "<div class='Top'>"+ data +"</div>";}
+	render: function(data){
+	if(parseFloat(data)>=70){
+	return "<div class='topfive'>"+ data +"</div>";}
 
-else if ((parseFloat(data)>=50) && ((parseFloat(data)<70))){
-return "<div class='Great'>"+ data +"</div>";}
+	else if (parseFloat(data)>=35){
+	return "<div class='QOne'>"+ data +"</div>";}
 
-else if ((parseFloat(data)>=30) && ((parseFloat(data)<50))){
-return "<div class='Good'>"+ data +"</div>";}
+	else if (parseFloat(data)>=15){
+	return "<div class='QTwo'>"+ data +"</div>";}
 
-else if ((parseFloat(data)>=10) && ((parseFloat(data)<30))){
-return "<div class='Ok'>"+ data +"</div>";}
+	else if (parseFloat(data)>=5){
+	return "<div class='QThree'>"+ data +"</div>";}
 
-else if ((parseFloat(data)>=0) && ((parseFloat(data)<10))){
-return "<div class='Poor'>"+ data +"</div>";}
+	else if (parseFloat(data)>=0){
+	return "<div class='QFour'>"+ data +"</div>";}
 return data }},
 
 
 // Letier //
 {targets:[14],
-render: function(data){
-if(parseFloat(data)>=80){
-return "<div class='Top'>"+ data +"</div>";}
+	render: function(data){
+	if(parseFloat(data)>=80){
+	return "<div class='topfive'>"+ data +"</div>";}
 
-else if ((parseFloat(data)>=65) && ((parseFloat(data)<80))){
-return "<div class='Great'>"+ data +"</div>";}
+	else if (parseFloat(data)>=65){
+	return "<div class='QOne'>"+ data +"</div>";}
 
-else if ((parseFloat(data)>=50) && ((parseFloat(data)<65))){
-return "<div class='Good'>"+ data +"</div>";}
+	else if (parseFloat(data)>=50){
+	return "<div class='QTwo'>"+ data +"</div>";}
 
-else if ((parseFloat(data)>=0) && ((parseFloat(data)<50))){
-return "<div class='Ok'>"+ data +"</div>";}
+	else if (parseFloat(data)>=0){
+	return "<div class='QThree'>"+ data +"</div>";}
 
 return data }},
 
 // dBQu //
 {targets:[15],
-render: function(data){
-if(parseFloat(data)>=4.5){
-return "<div class='Top'>"+ data +"</div>";}
+	render: function(data){
+	if(parseFloat(data)>=4.5){
+	return "<div class='topfive'>"+ data +"</div>";}
 
-else if ((parseFloat(data)>=4) && ((parseFloat(data)<4.5))){
-return "<div class='Great'>"+ data +"</div>";}
+	else if (parseFloat(data)>=4){
+	return "<div class='QOne'>"+ data +"</div>";}
 
-else if ((parseFloat(data)>=3.5) && ((parseFloat(data)<4))){
-return "<div class='Good'>"+ data +"</div>";}
+	else if (parseFloat(data)>=3.5){
+	return "<div class='QTwo'>"+ data +"</div>";}
 
-else if ((parseFloat(data)>=3) && ((parseFloat(data)<3.5))){
-return "<div class='Ok'>"+ data +"</div>";}
+	else if (parseFloat(data)>=3){
+	return "<div class='QThree'>"+ data +"</div>";}
 
-else if ((parseFloat(data)>=0) && ((parseFloat(data)<3))){
-return "<div class='Poor'>"+ data +"</div>";}
+	else if (parseFloat(data)>=0){
+	return "<div class='QFour'>"+ data +"</div>";}
 return data }},
 
 // dBAw //
 {targets:[16],
-render: function(data){
-if(parseFloat(data)>=95){
-return "<div class='Top'>"+ data +"</div>";}
+	render: function(data){
+	if(parseFloat(data)>=90){
+	return "<div class='topfive'>"+ data +"</div>";}
 
-else if ((parseFloat(data)>=90) && ((parseFloat(data)<95))){
-return "<div class='Great'>"+ data +"</div>";}
+	else if (parseFloat(data)>=95){
+	return "<div class='QOne'>"+ data +"</div>";}
 
-else if ((parseFloat(data)>=80) && ((parseFloat(data)<90))){
-return "<div class='Good'>"+ data +"</div>";}
+	else if (parseFloat(data)>=80){
+	return "<div class='QTwo'>"+ data +"</div>";}
 
-else if ((parseFloat(data)>=60) && ((parseFloat(data)<80))){
-return "<div class='Ok'>"+ data +"</div>";}
+	else if (parseFloat(data)>=60){
+	return "<div class='QThree'>"+ data +"</div>";}
 
-else if ((parseFloat(data)>=0) && ((parseFloat(data)<60))){
-return "<div class='Poor'>"+ data +"</div>";}
+	else if (parseFloat(data)>=0){
+	return "<div class='QFour'>"+ data +"</div>";}
 return data }},
 
 // Acpt% //
 {targets:[17], type: emptystringtobottom,
-render: function(data){
-if(parseFloat(data)>=40){
-return "<div class='Poor'>"+ data +"</div>";}
+	render: function(data){
+	if(parseFloat(data)>=40){
+	return "<div class='QFour'>"+ data +"</div>";}
 
-else if ((parseFloat(data)>=20) && ((parseFloat(data)<40))){
-return "<div class='Ok'>"+ data +"</div>";}
+	else if (parseFloat(data)>=20){
+	return "<div class='QThree'>"+ data +"</div>";}
 
-else if ((parseFloat(data)>=10) && ((parseFloat(data)<20))){
-return "<div class='Good'>"+ data +"</div>";}
+	else if (parseFloat(data)>=10){
+	return "<div class='QTwo'>"+ data +"</div>";}
 
-else if ((parseFloat(data)>=5) && ((parseFloat(data)<10))){
-return "<div class='Great'>"+ data +"</div>";}
+	else if (parseFloat(data)>=5){
+	return "<div class='QOne'>"+ data +"</div>";}
 
-else if ((parseFloat(data)>=0) && ((parseFloat(data)<5))){
-return "<div class='Top'>"+ data +"</div>";}
+	else if (parseFloat(data)>=0){
+	return "<div class='topfive'>"+ data +"</div>";}
 return data }},
 
 // Subs //
 {targets:[18],
-render: function(data){
-if(parseFloat(data)>=600){
-return "<div class='Top'>"+ data +"</div>";}
+	render: function(data){
+	if(parseFloat(data)>=600){
+	return "<div class='topfive'>"+ data +"</div>";}
 
-else if ((parseFloat(data)>=200) && ((parseFloat(data)<600))){
-return "<div class='Great'>"+ data +"</div>";}
+	else if (parseFloat(data)>=200){
+	return "<div class='QOne'>"+ data +"</div>";}
 
-else if ((parseFloat(data)>=100) && ((parseFloat(data)<200))){
-return "<div class='Good'>"+ data +"</div>";}
+	else if (parseFloat(data)>=100){
+	return "<div class='QTwo'>"+ data +"</div>";}
 
-else if ((parseFloat(data)>=0) && ((parseFloat(data)<100))){
-return "<div class='Ok'>"+ data +"</div>";}
+	else if (parseFloat(data)>=0){
+	return "<div class='QThree'>"+ data +"</div>";}
 return data }},
-
+	
 ]}
 
 var deeplink = $.fn.dataTable.ext.deepLink( [ 'search.search', 'order', 'displayStart', 'searchCols'] );
